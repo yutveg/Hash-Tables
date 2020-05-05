a9380018a4ff6e8cdc7d0a2a2962216a78612a64
yutveg <kodi.0n31l@gmail.com>
2020-05-05 09:00:49 -0700

    committing test py changes

--- test.py
+++ test.py
@@ -15,16 +15,20 @@ def min_sum(arr):
   if len(arr) == 1 and not isinstance(arr[0], list):
     return min(arr)
   else:
+
     # iterate through arr
     for item in arr:
+
       # if item is a list, recurse 
       if isinstance(item, list):
         print(item)
         total += min_sum(item)
+
       # if item is an int, add to int_list
       elif isinstance(item, int):
         int_list.append(item)
         print("int_list", int_list)
+        
     # add minimum value of int_list to total
     total += min(int_list) if len(int_list) > 0 else 0
   return total
